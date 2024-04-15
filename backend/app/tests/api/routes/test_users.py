3f2338837149be95bb7d9c34b2f4c4dd37f2ef7f
Xavier Baró <xavierbaro@gmail.com>
2024-04-15 19:01:59 +0200

    Tests cleaned

--- backend/app/tests/api/routes/test_users.py
+++ backend/app/tests/api/routes/test_users.py
@@ -128,20 +128,6 @@ def test_create_user_existing_username(
     assert "_id" not in created_user
 
 
-def test_create_user_by_normal_user(
-    client: TestClient, normal_user_token_headers: dict[str, str]
-) -> None:
-    username = random_email()
-    password = random_lower_string()
-    data = {"email": username, "password": password}
-    r = client.post(
-        f"{settings.API_V1_STR}/users/",
-        headers=normal_user_token_headers,
-        json=data,
-    )
-    assert r.status_code == 400
-
-
 def test_retrieve_users(
     client: TestClient, superuser_token_headers: dict[str, str], db: Session
 ) -> None:
