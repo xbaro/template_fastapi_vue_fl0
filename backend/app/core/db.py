e816ce946f9339641fdffcae3c489015b7fa509b
Xavier Baró <xavierbaro@gmail.com>
2024-04-17 01:38:15 +0200

    Fixed error with User tests

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -33,4 +33,4 @@ def init_db(session: Session) -> None:
             password=settings.FIRST_SUPERUSER_PASSWORD,
             is_superuser=True,
         )
-        user = crud.create_user(session=session, user_create=user_in)
+        user = crud.user.create_user(session=session, user_create=user_in)
--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -37,12 +37,6 @@ class UpdatePassword(SQLModel):
     current_password: str
     new_password: str
 
-
-# Database model, database table inferred from class name
-class User(UserBase, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    hashed_password: str
-
 # Properties to return via API, id is always required
 class UserOut(UserBase):
     id: int
